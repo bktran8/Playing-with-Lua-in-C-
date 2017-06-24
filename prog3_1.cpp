b70222817d688c82a75f2bc7bab7a08370eb1283
Brenda <bktran8@gmail.com>
2017-06-24 13:54:47 -0700

    add prog3_1

--- prog3_1.cpp
+++ prog3_1.cpp
@@ -14,7 +14,8 @@ int main( int argc, char *argv[]){
     }
     lua_State *One = luaL_newstate();
     luaL_openlibs(One);
-    luaL_dostring(One, "print('Hello World - How are you?')");
+    luaL_dofile(One, argv[1]);
+    //luaL_dostring(One, "print('Hello World - How are you?')");
     lua_close(One);
     return 0;
 }
