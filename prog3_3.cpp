e37889160a5d4a7473051b741e36646c00c1719a
Brenda <bktran8@gmail.com>
2017-06-26 14:04:32 -0700

    add prog3_3

--- prog3_3.cpp
+++ prog3_3.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <stdio.h>
+extern "C"{
+#include <iostream>
 
 extern "C"{
 #include "lua.h"
@@ -8,18 +11,14 @@ extern "C"{
 using namespace std;
 
 int main( int argc, char *argv[]){
-    char str[256];
-    cout << "Assignment #3-3, Brenda Tran, bktran8@gmail.com" << endl;
+    cout << "Assignment #3-1, Brenda Tran, bktran8@gmail.com" << endl;
     if(argc >= 3 || argc <=1){
         cerr << "ERROR! Program accepts 1 command line argument." << endl;
     }
-    cin.getline(str, sizeof(str));
-    
+
     lua_State *One = luaL_newstate();
     luaL_openlibs(One);
     luaL_dofile(One, argv[1]);
-    answer = InfixToPostfix(str);
-    printf(answer);
     lua_close(One);
     return 0;
 }
