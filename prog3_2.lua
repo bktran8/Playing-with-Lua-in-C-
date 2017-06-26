6ffde7f0be9a794be932c7b59ad97a0041e9ffe6
Brenda <bktran8@gmail.com>
2017-06-26 02:14:35 -0700

    add prog3_2

--- prog3_2.lua
+++ prog3_2.lua
@@ -1,21 +1,20 @@
-
 InfixToPostfix = function (str)
-    infix = {}; i = 1
-    for s in string.gmatch("%S+") do
-        infix[i] = s
-        i = i + 1
+    infix={};i=1
+    for s in str:gmatch("%S+") do
+        infix[i]=s
+        i=i+1
     end
     
-    postfix = {}; p = 1
-    stack = {}; s = 1
+    postfix={};p=1
+    stack={};s=1
     for k, v in pairs(infix) do
         if tonumber(v) ~= nil then
-            postfix[p] = v
-            p = p + 1
-        elseif v == "*" or v == "/" or v == "+" or v == "-" then
-            if stack[s] == nil then
-                stack[s] = v
-            elseif ((stack[s] == "+" and v == "-") or (stack[s] == "*" and v == "/")) then
+            postfix[p]=v
+            p=p+1
+        elseif (v=="*" or v=="/" or v=="+" or v=="-") then
+            if (stack[s]==nil) then
+                stack[s]=v
+            elseif ((stack[s]=="+" and v=="-") or (stack[s]=="*" and v=="/")) then
                 postfix[p] = stack[s]
                 p = p + 1
                 stack[s] = v
